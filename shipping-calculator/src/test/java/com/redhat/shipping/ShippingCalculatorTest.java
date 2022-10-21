aae2afc3f17c92abfa17a488ba96b26de5783dce
Christian Ferro <ecferro79@gmail.com>
2022-10-21 08:42:25 -0400

    Refactor

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -7,4 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ShippingCalculatorTest {
 
     // @todo: add tests
+
+@Test
+public void canCalculateTheCostForARegion() {
+	ShippingCalculator calculator = new ShippingCalculator();
+	assertEquals(0, calculator.costForRegion("A Region"));
+}
 }
